db3312a773d38070747e7248565905d9d34a8e3e
nurullah <nurullah7733@gmail.com>
2025-05-09 10:12:01 +0600

    db.ts modify

--- src/utils/supabase/db.ts
+++ src/utils/supabase/db.ts
@@ -3,9 +3,9 @@ const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.SUPABASE_DB_CONNECTION_STRING,
-  ssl: { rejectUnauthorized: false },
-  statement_timeout: 5000,
-  query_timeout: 5000,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
