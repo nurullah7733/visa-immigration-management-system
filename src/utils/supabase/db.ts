9f048d3e37f61af22228c62fb8dd09ad614d4ad9
nurullah <nurullah7733@gmail.com>
2025-05-09 09:41:50 +0600

    Force IPv4 add in pg pool

--- src/utils/supabase/db.ts
+++ src/utils/supabase/db.ts
@@ -3,6 +3,10 @@ const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.SUPABASE_DB_CONNECTION_STRING,
+
+  host: "db.czfqngofyvteucduxsmw.supabase.co",
+  port: 5432,
+
   ssl: { rejectUnauthorized: false },
 });
 
